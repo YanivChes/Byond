882205a49c38de15c3925305ce858285630309cc
Yaniv <yanivy@moveo.co.il>
2020-02-06 18:32:27 +0200

    RayCasting

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SelectionManager : MonoBehaviour
+{
+
+    private AudioSource audio;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.volume = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Selectable")
+        {
+            audio.volume = 0.4f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Selectable")
+        {
+            audio.volume = 0;
+        }
+    }
+}
